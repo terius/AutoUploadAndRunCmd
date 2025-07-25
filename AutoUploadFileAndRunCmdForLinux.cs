b3f4c2e2f529bacdeb10d4c3cb0c0f443100eddf
teriusstar <18728856@qq.com>
2025-07-25 09:16:19 +0800

    1.设置不允许启动多个程序。 2.解决长时间运行后无法连接到远程地址的bug

--- AutoUploadFileAndRunCmdForLinux.cs
+++ AutoUploadFileAndRunCmdForLinux.cs
@@ -1,5 +1,6 @@
 ﻿using Renci.SshNet;
 using System.Configuration;
+using System.Diagnostics;
 using System.Net.Sockets;
 
 namespace AutoUploadToFTP
@@ -15,7 +16,9 @@ namespace AutoUploadToFTP
         readonly int _sshPort;
         readonly int _ftpPort;
         readonly bool _uploadAllFileWhenFirstScan;
+        readonly string[] _ignorePathList;
         DateTime _checkTime;
+        int _tryTime = 5;
 
 
         Dictionary<string, DateTime> _checkFileTime = new();
@@ -24,9 +27,21 @@ namespace AutoUploadToFTP
         SshClient _sshClient = null;
         bool hasChange = true;
         bool isFirst = true;
-        public AutoUploadFileAndRunCmdForLinux(string host, string username,
-            string password, string localDirectory,
-            string remoteDirectory, string cmd, DateTime checkTime, int sshPort = 22, int ftpPort = 22, bool uploadAll = false)
+        string _localCmd;
+        public AutoUploadFileAndRunCmdForLinux(
+            string host,
+            string username,
+            string password,
+            string localDirectory,
+            string remoteDirectory,
+            string cmd,
+            DateTime checkTime,
+            int sshPort = 22,
+            int ftpPort = 22,
+            bool uploadAll = false,
+            string ignorePath = "",
+            string localCmd = null
+            )
         {
 
             _host = host;
@@ -39,6 +54,8 @@ namespace AutoUploadToFTP
             _ftpPort = ftpPort;
             _uploadAllFileWhenFirstScan = uploadAll;
             _checkTime = checkTime;
+            _ignorePathList = string.IsNullOrEmpty(ignorePath) ? null : ignorePath.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries); ;
+            _localCmd = localCmd;
         }
 
         private void WriteLogAndConsole(string message)
@@ -47,8 +64,33 @@ namespace AutoUploadToFTP
             Log.Write(message);
         }
 
+        private bool ConnectToFtp()
+        {
+            var isConnected = false;
+            try
+            {
+                if (_ftpClient != null && _ftpClient.IsConnected)
+                {
+                    return true;
+                }
+                WriteLogAndConsole($"Start establishing an FTP connection to server {_host}");
+                _ftpClient = new SftpClient(_host, _ftpPort, _username, _password);
+                _ftpClient.Connect();
+                isConnected = _ftpClient.IsConnected;
+                var connectMsg = isConnected ? "successful" : "failed";
+                WriteLogAndConsole($"FTP connection {connectMsg} ");
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                WriteLogAndConsole($"FTP connection failed,error:{ex.Message}");
+            }
+            return isConnected;
+        }
+
         public void Run()
         {
+            WriteLogAndConsole($"Start monitoring folders: {_localDirectory}\r\nRemote server: {_host}\r\nSSH Port: {_sshPort},FTP Port: {_ftpPort}");
             if (string.IsNullOrWhiteSpace(_localDirectory))
             {
                 WriteLogAndConsole($"Local directory can not be empty!");
@@ -58,6 +100,14 @@ namespace AutoUploadToFTP
             if (string.IsNullOrWhiteSpace(_remoteDirectory))
             {
                 WriteLogAndConsole($"Remote directory can not be empty!");
+                return;
+            }
+
+
+
+            var isConnected = ConnectToFtp();
+            if (!isConnected)
+            {
                 return;
             }
 
@@ -84,7 +134,7 @@ namespace AutoUploadToFTP
             // 启动监视
             watcher.EnableRaisingEvents = true;
 
-            WriteLogAndConsole($"Start monitoring folders {_localDirectory}");
+          
 
 
 
@@ -93,22 +143,25 @@ namespace AutoUploadToFTP
             {
                 if (!hasChange)
                 {
-                    Thread.Sleep(10);
+                    Thread.Sleep(1000);
                     continue;
                 }
-            
+
                 if (!isFirst)
                 {
-                    Thread.Sleep(5000);
+                    Thread.Sleep(3000);
                 }
                 if (uploadResult)
                 {
                     _newFiles.Clear();
                 }
+                RunLocalCmd(_localCmd);
 
                 ScanDir(rootDir);
                 if (_newFiles.Where(d => d.FileType != 2).ToList().Count > 0)
                 {
+                    var actionTime = DateTime.Now.AddMinutes(-1).ToString("HH:mm");
+
                     WriteLogAndConsole("\r\nFiles changes detected...");
                     foreach (var file in _newFiles)
                     {
@@ -116,8 +169,17 @@ namespace AutoUploadToFTP
                     }
 
 
-
-                    uploadResult = UploadFileUsingSsh();
+                    
+                    for (int i = 0; i < 5; i++)
+                    {
+                        uploadResult = UploadFileUsingSsh();
+                        if (uploadResult)
+                        {
+                            break;
+                        }
+                        CloseFTP();
+                    }
+                   
                     if (uploadResult)
                     {
                         Thread.Sleep(3000);
@@ -130,18 +192,19 @@ namespace AutoUploadToFTP
                         {
                             failFile.FileType = 2;
                         }
+                        SetCheckTimeValue(actionTime);
                     }
                 }
                 hasChange = false;
                 WriteLogAndConsole($"Total number of files monitored is {_checkFileTime.Count}");
                 if (isFirst)
                 {
-                    SetCheckTimeIsEmpty();
+                    SetCheckTimeValue();
                 }
                 isFirst = false;
-            
+
                 Thread.Sleep(3000);
-           
+
             }
         }
 
@@ -158,10 +221,27 @@ namespace AutoUploadToFTP
             }
         }
 
+        private bool CheckInIgnorePath(string path)
+        {
+            if (_ignorePathList == null)
+            {
+                return false;
+            }
+            foreach (var file in _ignorePathList)
+            {
+                if (path.Contains(file, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
         void ScanDir(Dir dir, string pathName = null)
         {
             var path = dir.Name;
-            if (!Directory.Exists(path))
+            if (!Directory.Exists(path) || CheckInIgnorePath(path))
             {
                 return;
             }
@@ -221,25 +301,29 @@ namespace AutoUploadToFTP
 
         }
 
-        bool UploadFileUsingSsh(int tryTimes = 5)
+        bool UploadFileUsingSsh()
         {
+            //if (_tryTime-- <= 0)
+            //{
+            //    Console.WriteLine($"Try times exceeded");
+            //    return false;
+            //}
             bool result = false;
             try
             {
+                ConnectToFtp();
+                //if (_ftpClient == null || !_ftpClient.IsConnected)
+                //{
+                //    WriteLogAndConsole("Start establishing an FTP connection to server...");
 
-
-                if (_ftpClient == null || !_ftpClient.IsConnected)
-                {
-                    WriteLogAndConsole($"Start establishing an FTP connection to server:{_host}...");
-
-                    _ftpClient = new SftpClient(_host, _ftpPort, _username, _password);
-                    _ftpClient.Connect();
-                    WriteLogAndConsole("FTP connection successful");
-                }
+                //    _ftpClient = new SftpClient(_host, _ftpPort, _username, _password);
+                //    _ftpClient.Connect();
+                //    WriteLogAndConsole("FTP connection successful");
+                //}
 
                 if (_ftpClient.IsConnected)
                 {
-                    WriteLogAndConsole($"Start sending files to the server:{_host}");
+                    WriteLogAndConsole("Start sending files to  server");
                     bool hasError = false;
                     HashSet<string> createPathList = new();
                     foreach (var file in _newFiles)
@@ -258,8 +342,7 @@ namespace AutoUploadToFTP
                         catch (SocketException socketExp)
                         {
                             WriteLogAndConsole($"File {file} upload failed, reason: {socketExp.Message}, started trying to reconnect...");
-                            CloseFTP();
-                            hasError = !UploadFileUsingSsh(tryTimes);
+                            hasError = true;
                             break;
                         }
                         catch (Exception ex)
@@ -267,6 +350,7 @@ namespace AutoUploadToFTP
                             WriteLogAndConsole($"File {file} upload failed, reason:{ex.Message}");
                             hasError = true;
                             break;
+
                         }
 
                     }
@@ -282,18 +366,7 @@ namespace AutoUploadToFTP
             catch (Exception ex)
             {
                 WriteLogAndConsole($"FTP connection error occurred，reason：{ex.Message}");
-                tryTimes--;
-                if (tryTimes <= 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    UploadFileUsingSsh(tryTimes);
-                }
-
-
-                return false;
+                result = false;
             }
 
             return result;
@@ -309,7 +382,7 @@ namespace AutoUploadToFTP
             }
             if (_sshClient == null || !_sshClient.IsConnected)
             {
-                WriteLogAndConsole($"Start establishing an SSH connection to server:{_host}...");
+                WriteLogAndConsole("Start establishing an SSH connection to server...");
                 _sshClient = new SshClient(_host, _sshPort, _username, _password);
                 _sshClient.Connect();
                 WriteLogAndConsole($"SSH connection successful");
@@ -317,7 +390,7 @@ namespace AutoUploadToFTP
 
             if (_sshClient.IsConnected)
             {
-                WriteLogAndConsole($"Start executing commands:{cmd} on server:{_host}");
+                WriteLogAndConsole($"Start executing commands:{cmd}");
                 // 执行远程命令
                 var command = _sshClient.RunCommand(cmd);
 
@@ -340,7 +413,57 @@ namespace AutoUploadToFTP
             return false;
         }
 
-        private void SetCheckTimeIsEmpty()
+        public bool RunLocalCmd(string cmd = null)
+        {
+            if (string.IsNullOrWhiteSpace(cmd))
+            {
+                return true;
+            }
+            //// 源路径和目标路径
+            //string sourcePath = @"D:\work\serverpublish\appsettings.Production.json";
+            //  string destinationPath = @"C:\Users\Pingbin\Desktop\temp\0522";
+
+            //// xcopy 命令参数
+            // string xcopyArgs = $@"""{sourcePath}"" ""{destinationPath}"" /Y /R /C /H";
+
+            Console.WriteLine($"Begin Run LocalCommand:{cmd}");
+            // 创建并配置 ProcessStartInfo
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {cmd}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // 启动进程
+            using (Process process = new Process { StartInfo = processInfo })
+            {
+                process.Start();
+
+                // 读取输出和错误流
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                // 等待进程结束
+                process.WaitForExit();
+
+
+
+                if (!string.IsNullOrWhiteSpace(error))
+                {
+                    Console.WriteLine($"Error:{error}");
+                    return false;
+                }
+                Console.WriteLine($"Output:{output}");
+                return true;
+
+            }
+        }
+
+        private void SetCheckTimeValue(string timeValue = "")
         {
             Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
@@ -348,7 +471,7 @@ namespace AutoUploadToFTP
             AppSettingsSection appSettings = config.AppSettings;
 
             // 修改键值对
-            appSettings.Settings["CheckTime"].Value = "";
+            appSettings.Settings["CheckTime"].Value = timeValue;
 
             // 保存修改
             config.Save(ConfigurationSaveMode.Modified);
--- Program.cs
+++ Program.cs
@@ -1,49 +1,77 @@
 ﻿namespace AutoUploadToFTP;
 class Program
 {
-
+    // private const string AppMutexName = "Global\\{8AA7CD65-FD7F-4F97-BE11-1A59E2B16E18}";
     static void Main()
     {
-        string host = GetAppSettingValue("Host");
-        string username = GetAppSettingValue("Username") ?? "root";
-        string password = GetAppSettingValue("Password");
-        string sshPortStr = GetAppSettingValue("Sshport");
-        string ftpPortStr = GetAppSettingValue("Ftpport");
-        int sshPort = 22;
-        if (!string.IsNullOrWhiteSpace(sshPortStr))
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory.Replace("\\","");
+        var appMutexName = "Global\\{" + baseDirectory + "}";
+        Mutex mutex = new Mutex(true, appMutexName, out bool createdNew);
+
+        if (!createdNew)
         {
-            sshPort = int.Parse(sshPortStr);
+            // 如果互斥体已存在，则说明程序已在运行
+            Console.WriteLine("应用程序已经在运行了。按任意键退出。");
+            Console.ReadKey();
+            return; // 退出当前实例
         }
-        int ftpPort = 22;
-        if (!string.IsNullOrWhiteSpace(ftpPortStr))
+
+        // 3. 确保在程序退出时释放互斥体
+        // 使用 try...finally 结构可以保证即使程序发生异常，互斥体也能被正确释放。
+        try
         {
-            ftpPort = int.Parse(ftpPortStr);
+            string host = GetAppSettingValue("Host");
+            string username = GetAppSettingValue("Username") ?? "root";
+            string password = GetAppSettingValue("Password");
+            string sshPortStr = GetAppSettingValue("Sshport");
+            string ftpPortStr = GetAppSettingValue("Ftpport");
+            int sshPort = 22;
+            if (!string.IsNullOrWhiteSpace(sshPortStr))
+            {
+                sshPort = int.Parse(sshPortStr);
+            }
+            int ftpPort = 22;
+            if (!string.IsNullOrWhiteSpace(ftpPortStr))
+            {
+                ftpPort = int.Parse(ftpPortStr);
+            }
+            string localFilePath = GetAppSettingValue("Localpath");
+            string remoteDirectory = GetAppSettingValue("Remotepath");
+            string cmd = GetAppSettingValue("Cmd");
+            bool uploadAllFilesWhenFirstScan = GetAppSettingValue("UploadAllFilesWhenAppStart") == "1";
+            var checkTime = DateTime.Now;
+            var sTimeStr = GetAppSettingValue("CheckTime");
+            if (!string.IsNullOrWhiteSpace(sTimeStr))
+            {
+                var timeList = sTimeStr.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                if (timeList.Length == 3)
+                {
+                    checkTime = DateTime.Today.AddHours(double.Parse(timeList[0])).AddMinutes(double.Parse(timeList[1])).AddSeconds(double.Parse(timeList[2]));
+                }
+                else if (timeList.Length == 2)
+                {
+                    checkTime = DateTime.Today.AddHours(double.Parse(timeList[0])).AddMinutes(double.Parse(timeList[1]));
+                }
+                else if (timeList.Length == 1)
+                {
+                    checkTime = DateTime.Today.AddHours(double.Parse(timeList[0]));
+                }
+            }
+            var ignorePath = GetAppSettingValue("IgnorePath");
+            var localCmd = GetAppSettingValue("LocalCmd");
+            var autoRun = new AutoUploadFileAndRunCmdForLinux(host, username, password, localFilePath, remoteDirectory,
+                cmd, checkTime, sshPort, ftpPort, uploadAllFilesWhenFirstScan, ignorePath, localCmd);
+            autoRun.Run();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
-        string localFilePath = GetAppSettingValue("Localpath");
-        string remoteDirectory = GetAppSettingValue("Remotepath");
-        string cmd = GetAppSettingValue("Cmd");
-        bool uploadAllFilesWhenFirstScan = GetAppSettingValue("UploadAllFilesWhenAppStart") == "1";
-        var checkTime = DateTime.Now;
-        var sTimeStr = GetAppSettingValue("CheckTime");
-        if (!string.IsNullOrWhiteSpace(sTimeStr))
+        finally
         {
-            var timeList = sTimeStr.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            if (timeList.Length == 3)
-            {
-                checkTime = DateTime.Today.AddHours(double.Parse(timeList[0])).AddMinutes(double.Parse(timeList[1])).AddSeconds(double.Parse(timeList[2]));
-            }
-            else if (timeList.Length == 2)
-            {
-                checkTime = DateTime.Today.AddHours(double.Parse(timeList[0])).AddMinutes(double.Parse(timeList[1]));
-            }
-            else if (timeList.Length == 1)
-            {
-                checkTime = DateTime.Today.AddHours(double.Parse(timeList[0]));
-            }
+            // 释放互斥体，允许其他实例在本次运行结束后启动
+            mutex.ReleaseMutex();
+            mutex.Dispose();
         }
-        var autoRun = new AutoUploadFileAndRunCmdForLinux(host, username, password, localFilePath, remoteDirectory,
-            cmd, checkTime, sshPort, ftpPort, uploadAllFilesWhenFirstScan);
-        autoRun.Run();
+
 
     }
 
